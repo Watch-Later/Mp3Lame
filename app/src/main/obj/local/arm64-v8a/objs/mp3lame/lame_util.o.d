28488056234569776ea14841d52402d33cc6cfee
wangguoqun <kuyuzhiqi@kuyuMbp.local>
2019-06-29 16:07:24 +0800

    mp3编码

---
+++ app/src/main/obj/local/arm64-v8a/objs/mp3lame/lame_util.o.d
@@ -0,0 +1,6 @@
+./obj/local/arm64-v8a/objs/mp3lame/lame_util.o: jni/lame_util.c \
+  jni/lame_3.99.5_libmp3lame/lame.h jni/com_lame_mp3_utils_LameUtil.h
+
+jni/lame_3.99.5_libmp3lame/lame.h:
+
+jni/com_lame_mp3_utils_LameUtil.h:
---
+++ app/src/main/obj/local/x86_64/objs/mp3lame/lame_util.o.d
@@ -0,0 +1,6 @@
+./obj/local/x86_64/objs/mp3lame/lame_util.o: jni/lame_util.c \
+  jni/lame_3.99.5_libmp3lame/lame.h jni/com_lame_mp3_utils_LameUtil.h
+
+jni/lame_3.99.5_libmp3lame/lame.h:
+
+jni/com_lame_mp3_utils_LameUtil.h:
